1a8f85f210acd621da86cfd928a6429e2c99194a
Your Name <you@example.com>
2020-11-24 15:37:26 -0500

    1

--- src/scripts/docker/start.sh
+++ src/scripts/docker/start.sh
@@ -1,21 +1,7 @@
 #!/bin/sh
 
 
-# Copy config file if it doesn't exist so that the app can start
-if [ -f /app/config/mounted/config.cfg ] ; then
-  echo "Mounted Config file found at /app/config/config.cfg"
-  echo "Copying configuration file to run."
-  cp /app/config/mounted/config.cfg /app/config/config.cfg || exit 1
- fi
-
-# Copy config file if it doesn't exist so that the app can start
-if [ ! -f /app/config/config.cfg ] ; then
-  echo "Config file not found at /app/config/config.cfg"
-  echo "Copying example configuration file to run."
-  cp /app/docker/default-config.cfg /app/config/config.cfg || exit 1
- fi
-
-
+function dorun(){
 echo "";
 echo "Version and build information:";
 echo "";
@@ -29,8 +15,22 @@ echo "Starting GLauth now.";
 echo "";
 
 # Run app
-/app/glauth -c /app/config/config.cfg
-
+/app/glauth -c $1
 echo ""
 echo "GLauth has exited."
 echo "Exiting."
+}
+
+# Copy config file if it doesn't exist so that the app can start
+if [ -f /app/config/mounted/config.cfg ] ; then
+  echo "Mounted Config file found at /app/config/config.cfg"
+  echo "Copying configuration file to run."
+  dorun /app/config/mounted/config.cfg   || exit 1
+ fi
+
+# Copy config file if it doesn't exist so that the app can start
+if [ ! -f /app/config/config.cfg ] ; then
+  echo "Config file not found at /app/config/config.cfg"
+  echo "Copying example configuration file to run."
+  dorun /app/docker/default-config.cfg  || exit 1
+ fi
